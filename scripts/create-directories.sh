b2c35e66c5b7a5a8b96716ded7b99ff9e9bb5621
Hishiro64 <114261822+Hishiro64@users.noreply.github.com>
2026-04-10 07:24:10 -0400

    Update directory creation in create-directories.sh

--- scripts/create-directories.sh
+++ scripts/create-directories.sh
@@ -10,7 +10,7 @@ mkdir -p /srv/stacks
 
 cd /srv/stacks
 
-mkdir -p {portainer,homer,wg-easy,file-browser/branding,jellyfin/{config,media/{movies,shows,anime,music,restricted}},kavita/{config,data/{light-novels,books}},jellyseerr,languagetool/ngrams,syncthing,aria2-pro/{config,downloads},qbittorrent/{config,downloads},pi-hole/{etc_pihole,etc_dnsmasq},pi.alert/config,gitea/data,stash/{config/scrapers,data,metadata,cache,blobs,generated},netdata/netdataconfig,samba/{data,Serva},scrutiny/config,duplicacy/{config,backup,restore},qdirstat/config}
+mkdir -p {aria2-pro/{config,downloads},calibre-web-automated/{config,cwa-book-ingest,calibre-library},duplicacy/{config,backup,restore},file-browser/branding,gitea/data,homer,immich/{library,postgres},jellyfin/{config,media/{movies,shows,anime,music,restricted}},jellyseerr,kavita/{config,data/{light-novels,books}},kiwix-serve/zim,lanraragi/content,languagetool/ngrams,memos,netdata/netdataconfig,pi-hole/{etc_pihole,etc_dnsmasq},pi.alert/config,portainer,qbittorrent/{config,downloads},qdirstat/config,samba/{data,Serva},scrutiny/config,seerr,snippet-box/data,stash/{config/scrapers,data,metadata,cache,blobs,generated},syncthing,wg-easy}
 
 # File Browser
 cd ./file-browser
